f4cb64d934c83a5051ed70dcb5214ca759fd1be0
azu <azuciao@gmail.com>
2024-05-16 01:08:02 +0900

    init

---
+++ .pnpm-run.mjs
@@ -0,0 +1,41 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { execSync } from "child_process";
+
+if (process.env.PNPM_RUN_HOOK_INSTALLATION === 'true') {
+    console.log("Hook installation is done");
+    process.exit(0);
+}
+
+const __dirname = import.meta.dirname;
+const installedLockLockFile = path.join(__dirname, 'node_modules/.pnpm/lock.yaml');
+
+const projectLockFile = path.join(__dirname, 'pnpm-lock.yaml');
+const shouldPnpmInstall = () => {
+    // not exists node_modules
+    if (!fs.existsSync(path.join(__dirname, 'node_modules'))) {
+        return true;
+    }
+    // not exists lockfile
+    if (!fs.existsSync(installedLockLockFile)) {
+        return true;
+    }
+    // lockfile is different
+    if (fs.existsSync(projectLockFile) && fs.existsSync(installedLockLockFile)) {
+        return fs.readFileSync(projectLockFile, 'utf8') !== fs.readFileSync(installedLockLockFile, 'utf8');
+    }
+    return false;
+}
+// if the lockfile is different from the one that was installed, we need to update the installed lockfile
+if (shouldPnpmInstall()) {
+    console.log("Lockfile is different, updating the installed lockfile");
+    // execute pnpm install to update the lockfile
+    execSync('pnpm install', {
+        stdio: 'inherit',
+        env: {
+            PNPM_RUN_HOOK_INSTALLATION: 'true',
+        }
+    });
+}
+
+console.log(process.argv);
